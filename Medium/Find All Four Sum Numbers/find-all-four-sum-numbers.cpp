a7733fa4b5d713b3681bae8ddedfecb326152203
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-05-26 15:19:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find All Four Sum Numbers/find-all-four-sum-numbers.cpp
+++ Medium/Find All Four Sum Numbers/find-all-four-sum-numbers.cpp
@@ -11,43 +11,37 @@ class Solution{
     // arr[] : int input array of integers
     // k : the quadruple sum required
     vector<vector<int> > fourSum(vector<int> &nums, int target) {
-        int n = nums.size(); //size of the array
-        vector<vector<int>> ans;
-    
-        // sort the given array:
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-    
-        //calculating the quadruplets:
+        vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
-            // avoid the duplicates while moving i:
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i - 1] == nums[i])
+                continue;
             for (int j = i + 1; j < n; j++) {
-                // avoid the duplicates while moving j:
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-    
-                // 2 pointers:
+                if (j > i + 1 && nums[j - 1] == nums[j])
+                    continue;
                 int k = j + 1;
                 int l = n - 1;
                 while (k < l) {
-                    long long sum = nums[i];
-                    sum += nums[j];
+                    long long sum = nums[i] + nums[j];
                     sum += nums[k];
                     sum += nums[l];
-                    if (sum == target) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
-                        ans.push_back(temp);
-                        k++; l--;
-    
-                        //skip the duplicates:
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    if (sum < target) {
+                        k++;
+                    } else if (sum > target) {
+                        l--;
+                    } else {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        while (k < l && nums[k - 1] == nums[k])
+                            k++;
+                        while (k < l && nums[l + 1] == nums[l])
+                            l--;
                     }
-                    else if (sum < target) k++;
-                    else l--;
                 }
             }
         }
-    
         return ans;
     }
 };
