32b08657c828a0020b2c87e6e74e0e05d6ec9312
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-05-29 18:46:06 +0530

    Update introduction-to-dp.cpp

--- Medium/Introduction to DP/introduction-to-dp.cpp
+++ Medium/Introduction to DP/introduction-to-dp.cpp
@@ -8,21 +8,22 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+const int MOD = 1e9 + 7;
+vector<long long> dp(10001,-1);
+
 class Solution {
   public:
     long long int topDown(int n) {
-        const int MOD = 1e9 + 7;
-        long long ans = 1;
-        long long prev = 0;
-        for (int i = 2; i <= n; i++){
-            int next = (ans + prev)%MOD;
-            prev = ans;
-            ans = next;
-        }
-        return ans%MOD;
+        //const int MOD = 1e9 + 7;
+        if(n <= 1) return n;
+          if(dp[n]!=-1) return dp[n];
+          long long add1 = topDown(n-1);
+          long long add2 =topDown(n-2);
+          return dp[n] = (add1 + add2)%MOD;
     }
+    
     long long int bottomUp(int n) {
-        const int MOD = 1e9 + 7;
+        //const int MOD = 1e9 + 7;
         long long ans = 1;
         long long prev = 0;
         for (int i = 2; i <= n; i++){
