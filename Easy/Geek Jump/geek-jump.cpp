b600b7684bf1cbfee26a349650d49729d14b1681
Abhishek Kumar Kushwaha <117573435+Abhishek-K-Kushwaha@users.noreply.github.com>
2024-05-30 13:09:29 +0530

    Update geek-jump.cpp

--- Easy/Geek Jump/geek-jump.cpp
+++ Easy/Geek Jump/geek-jump.cpp
@@ -7,23 +7,18 @@ using namespace std;
 class Solution {
   public:
     int minimumEnergy(vector<int>& h, int n) {
-        int enrg2 = abs(h[0] - h[1]);
-        int enrg1 = min(abs(h[0] - h[2]), abs(h[0] - h[1]) + abs(h[1] - h[2]));
-        if (n == 1) return 0;
-        if (n == 2) return enrg2;
-        if (n == 3) return enrg1;
-        
-        for (int i = 4; i <= n; i++){
+        int enrg2 = 0;
+        int enrg1 = abs(h[0] - h[1]);
+        if (n == 1) return enrg2;
+        if (n == 2) return enrg1;
+
+        for (int i = 3; i <= n; i++){
             int temp1 = enrg1 + abs(h[i-1] - h[i-2]);
             int temp2 = enrg2 + abs(h[i-3] - h[i-1]);
             enrg2 = enrg1;
             enrg1 = min(temp1,temp2);
         }
         return enrg1;
-        
-        // int enrg1 = minimumEnergy(h, n-1) + abs(h[n-1] - h[n-2]);
-        // int enrg2 = minimumEnergy(h, n-2) + min(abs(h[n-3] - h[n-1]), abs(h[n-3] - h[n-2]) + abs(h[n-2] - h[n-1]));
-        // return min(enrg1, enrg2);
     }
 };
 
